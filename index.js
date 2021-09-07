0d3b02c00e87b7dd28390c306f8d5ccd4721647f
KIKIWHYY <yougonnadie123@mail.ru>
2021-09-07 12:42:26 +0400

    discord.js init

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send("Hello world!"));
 app.listen(port, () => console.log(`Example app listening at https://localhost:${port}`));
+
+const { Client, Intents } = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const token = 'ODc1MTM3NTQwMTQ1OTQyNTI4.YRRJWA.xksj6dprMIa-aZ6j94hdteQnx-g'
+
+client.on('ready', () => {
+	console.log('ready!')
+})
+
+client.login(token)
