4bca6d42da23c26b49fbebe043598eadad69bf61
KIKIWHYY <yougonnadie123@mail.ru>
2021-09-08 17:26:09 +0400

    added express server

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+app.get('/', (req, res) => res.send("Hello world"))
+app.listen(port, () => console.log(`Example app listening at https://localhost:${port}`))
+
 const { MessageAttachment, Client, Intents } = require('discord.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 const fs = require('fs')
